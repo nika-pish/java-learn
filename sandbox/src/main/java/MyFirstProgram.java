df07331d95a096b4c49febb6f7d584f659667101
nika-pish <59174244+nika-pish@users.noreply.github.com>
2020-01-05 22:45:29 +0300

    Задание №2

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,17 @@
+import ru.stqa.learn.Point;
+
 public class MyFirstProgram {
-	public static void main (String[] args) {
-		System.out.println ("Hello, World");
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+
+
+		Point p1 = new Point(3, 5,6,8);
+
+
+		System.out.println("Расстояние между двумя точками = "+ p1.distance());
+
 	}
-} 
+
+
+}
+
---
+++ sandbox/src/main/java/ru/stqa/learn/sandbox.java
@@ -0,0 +1,7 @@
+package ru.stqa.learn;
+
+public class sandbox {
+    public static void main (String[] args) {
+        System.out.println ("Hello, World");
+    }
+}
