7e769471691f07d1d2a5f8914ccafcac76f4ff4b
nika-pish <59174244+nika-pish@users.noreply.github.com>
2020-01-07 17:59:44 +0300

    Задание №4 (Создание теста по добавлению нового контакта)

---
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/ContactData.java
@@ -0,0 +1,67 @@
+package ru.stqa.learn.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String middlename;
+    private final String lastname;
+    private final String nickname;
+    private final String address;
+    private final String mobile;
+    private final String email;
+    private final String bday;
+    private final String bmonth;
+    private final String byear;
+
+    public ContactData(String firstname, String middlename, String lastname, String nickname, String address, String mobile, String email, String bday, String bmonth, String byear) {
+        this.firstname = firstname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.nickname = nickname;
+        this.address = address;
+        this.mobile = mobile;
+        this.email = email;
+        this.bday = bday;
+        this.bmonth = bmonth;
+        this.byear = byear;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getBday() {
+        return bday;
+    }
+
+    public String getBmonth() {
+        return bmonth;
+    }
+
+    public String getByear() {
+        return byear;
+    }
+}
