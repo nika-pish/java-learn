08506e8d2830a09041e4fff56075a23f0c1f64db
nika-pish <59174244+nika-pish@users.noreply.github.com>
2020-01-28 23:35:32 +0300

    Оптимизированы переходы между страницами ("ленивые" действия")

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/appmanager/NavigationHelper.java
@@ -11,14 +11,26 @@ public class NavigationHelper extends HelperBase{
     }
 
     public void gotoGroupPage() {
-      click(By.linkText("groups"));
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+                && isElementPresent(By.name("new"))){
+            return;
+        }
+        click(By.linkText("groups"));
     }
 
     public void goToAddNewContactPage() {
+        if (isElementPresent(By.tagName("h1"))
+                && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+            return;
+        }
         click(By.linkText("add new"));
     }
 
     public void goToHomePage() {
+        if (isElementPresent(By.id ("maintable"))){
+            return;
+        }
         click(By.linkText("home"));
     }
 }
