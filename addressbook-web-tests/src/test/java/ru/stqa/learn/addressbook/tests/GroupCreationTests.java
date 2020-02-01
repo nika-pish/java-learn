efd41c8180ea4810189d1d2d8de5869ed790a624
nika-pish <59174244+nika-pish@users.noreply.github.com>
2020-02-01 14:53:05 +0300

    Провека по кол-ву элементов в коллекции (для групп)

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.learn.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.learn.addressbook.model.GroupData;
 
@@ -7,11 +8,10 @@ public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
-
     app.getNavigationHelper().gotoGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before +1);
   }
-
-
 }
