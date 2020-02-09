2735f089d44c94784e166b24c5d84e789f180882
nika-pish <59174244+nika-pish@users.noreply.github.com>
2020-02-09 18:07:53 +0300

    Лямбда-выражения (анонимные функции) и элементы функц. прогр-ния (контакт)

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/ContactCreationTests.java
@@ -3,7 +3,6 @@ package ru.stqa.learn.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.learn.addressbook.model.ContactData;
-import ru.stqa.learn.addressbook.model.GroupData;
 
 import java.util.HashSet;
 import java.util.List;
@@ -14,23 +13,16 @@ public class ContactCreationTests extends TestBase {
     public void testContactCreation() throws Exception {
         app.getNavigationHelper().goToHomePage();
         List<ContactData> before = app.getContactHelper().getContactList();
-        ContactData contact = new ContactData("Ivan", "Ivanov", "Ivanovich", "IvIv", "Moscow", "44555", "222443@fake.fake", "1", "January", "1990", "test1");
+        ContactData contact = new ContactData("Petr", "Ivanov", "Ivanovich", "IvIv", "Moscow", "44555", "222443@fake.fake", "1", "January", "1990", "test1");
         app.getNavigationHelper().goToAddNewContactPage();
         app.getContactHelper().createContact(contact);
         app.getNavigationHelper().goToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() +1);
 
-        int max = 0;
-        for (ContactData c : after) {
-            if (c.getId () > max) {
-                max = c.getId();
-            }
-        }
-        contact.setId(max);
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add (contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object> (after));
     }
-
 }
 
