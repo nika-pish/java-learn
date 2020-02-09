af020da9ad24494478513e107e1917a0b4ef3c20
nika-pish <59174244+nika-pish@users.noreply.github.com>
2020-02-09 19:33:42 +0300

    Реструктуризация кода (открытие браузера, иницилизация) (группы)

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/ContactDeletionTests.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class ContactDeletionTests extends TestBase {
 
-    @Test
+    @Test (enabled = false)
     public void testContactDeletion() throws Exception {
         app.getNavigationHelper().goToHomePage();
         if (! app.getContactHelper().isThereAContact()) {
