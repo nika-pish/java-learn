6dc28362ffa345c97f7cdec334f9880404610ab4
nika-pish <59174244+nika-pish@users.noreply.github.com>
2020-10-28 17:26:55 +0300

    Добавлено протоколирование запусков (лог-файлы)

--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.stqa.learn.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.learn.addressbook.model.GroupData;
@@ -18,6 +20,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
     try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))){
@@ -59,7 +62,6 @@ public class GroupCreationTests extends TestBase {
       Groups after = app.group().all();
       assertThat(after, equalTo(
               before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
   }
 
   @Test (enabled = false)
--- addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/learn/addressbook/tests/TestBase.java
@@ -1,24 +1,43 @@
 package ru.stqa.learn.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.learn.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
-    protected static final ApplicationManager app
-            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-    @BeforeSuite
-    public void setUp() throws Exception {
-        app.init();
-    }
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
-    @AfterSuite
-    public void tearDown() throws Exception {
-        app.stop();
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
 
-    }
+  @AfterSuite
+  public void tearDown() throws Exception {
+    app.stop();
+  }
 
+  @BeforeMethod(alwaysRun = true)
+  public void logTestStart(Method m, Object[] p) {
+      logger.info("Start test" + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+      logger.info("Stop test" + m.getName());
+  }
 }
+
+
