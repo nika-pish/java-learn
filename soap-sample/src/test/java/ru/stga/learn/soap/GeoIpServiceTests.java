ffb527f322193abe071b9ff32ffbe59675d7aa48
nika-pish <59174244+nika-pish@users.noreply.github.com>
2020-11-13 19:31:00 +0300

    Добавлен новый модуль soap-sample, создан тест GeoIpServiceTests

---
+++ soap-sample/src/test/java/ru/stga/learn/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package ru.stga.learn.soap;
+
+import com.lavasoft.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+  @Test
+  public void testMyIp() {
+    String ipLocation = new GeoIPService().getGeoIPServiceSoap12().getIpLocation("89.110.8.181");
+    System.out.println(ipLocation);
+  }
+}
